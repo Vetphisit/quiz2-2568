2827cc4e28ee6dd46024aa519944af5f8fb33222
Vetphisit <vetpisit07@gmail.com>
2025-09-08 11:40:54 +0700

    done

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
+
 import { Button, Stack, Title, Divider, Container } from "@mantine/core";
-import { v4 as uuidv4 } from "uuid";
-import { randomId } from "@mantine/hooks";
 
 
-type Expense = {
-  id: string;
-  name: string;
-  amount: number | string;
-  category: string;
-};
 
 export default function ExpenseTracker() {
-  const [opened, setOpened] = useState(false);
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-  const categories = ["Food", "Transport", "Entertainment"];
 
-  
  const registerBtnOnClick = () => {
 
  }
